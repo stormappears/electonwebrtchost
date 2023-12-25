46ee49302b2761e8bc4c440c559e8d2e81e7bbed
THIS IS NOT ME <92795413+stormappears@users.noreply.github.com>
2023-12-25 12:33:44 +0530

    Update App.js

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,24 @@
 import io from "socket.io-client";
 import Peer from "peerjs";
+
 import { useState, useRef, useEffect, createRef } from "react";
 import "./App.css";
 import { useScreenshot } from "use-react-screenshot";
-
+import { db } from "./firebase";
+import {
+  getDocs,
+  collection,
+  doc,
+  getFirestore,
+  deleteDoc,
+  onSnapshot,
+  setDoc,
+} from "firebase/firestore";
 const electron = window.electron;
 
+let firestoredb = getFirestore();
 //socket io connection address
+// const socket = io.connect("https://rd-server-2ako.onrender.com/");
 const socket = io.connect("https://ivoryleostarvipon.onrender.com/");
 
 // esablish new peer instance here we generate our id
@@ -42,23 +54,29 @@ function App() {
   const controllerPanel = useRef(null);
   const controllerPinRef = useRef(null);
   const videoarearef = useRef(null);
+  const selecctorBox = useRef();
+  const selecctorPin = useRef();
   const [isMouseOnPanel, setIsMouseOnPanel] = useState(false);
 
+  //firebase state
+  const [userList, setUserList] = useState([]);
+  const [isPeerBusy, setIsPeerBusy] = useState(false);
 
-
+  //firebase counter binding
+  let count = 1;
 
   // Join Room Function
 
   function joinRoom() {
-    console.log("Room Function Hitted")
+    console.log("Room Function Hitted");
     if (room !== "") {
       socket.emit("join_room", room);
       socket.emit("monoff", { room });
-        socket.emit("askscreen", { room });
-        console.log("asksed screen")
+      socket.emit("askscreen", { room });
+      console.log("asksed screen");
     }
     getStream2(room);
-  
+
     // elementr.style.cursor = 'none'
     // document.getElementById('remscreen').style.cursor = 'url(' + myCursor + ')';
     // document.getElementById("remscreen").style.cursor =  ' url("./assets/cursor.png") 24 24, auto'
@@ -70,33 +88,29 @@ function App() {
 
   //
 
-  //Mouse ClickL Event
-  // const handleMouseClick = ({ clientX, clientY }) => {
-  //   //scr element
-  //   // const screlement = document.getElementById('remscreen');
-  //   // const clientWidth = screlement.offsetWidth;
-  //   // const clientHeight = screlement.offsetHeight;
+  // Mouse ClickL Event
+  const handleMouseClick = ({ clientX, clientY }) => {
+    //scr element
+    // const screlement = document.getElementById('remscreen');
+    // const clientWidth = screlement.offsetWidth;
+    // const clientHeight = screlement.offsetHeight;
 
-  //   socket.emit("mouseclickl", {
-  //     clientX,
-  //     clientY,
-  //     // clientWidth,
-  //     // clientHeight,
-  //     clientWidth: window.innerWidth,
-  //     clientHeight: window.innerHeight,
-  //     room,
-  //   });
-  // };
-  
+    socket.emit("mouseclickl", {
+      clientX,
+      clientY,
+      // clientWidth,
+      // clientHeight,
+      clientWidth: window.innerWidth,
+      clientHeight: window.innerHeight,
+      room,
+      enableToggler,
+    });
+  };
 
-
-
-
-// ================================================================
+  // ================================================================
   // const handleMouseMove = ({ clientX, clientY }) => {
   //   let  clientWidth = videoarearef.current.clientWidth;
   //   let clientHeight = videoarearef.current.clientHeight;
-
 
   //   console.log(clientX + "Hey its working");
   //   socket.emit("mousecord", {
@@ -109,8 +123,6 @@ function App() {
   //     clientHeight,
   //   });
   // };
-
-
 
   //Mouse ClickR Event
 
@@ -149,63 +161,65 @@ function App() {
       console.log("mouse x : " + data.mousex + "mouse y : " + data.mousey);
     });
 
-      
-      //recive screen 
-      socket.on("recive_sendscreen", (data) => {
-        console.log("Screen Data Arived So Long");
-        console.log(data.boundHeight);
-        console.log(data.boundWidth);
-          let elementr = document.getElementById("remscreendiv");
-          elementr.style.height = data.boundHeight;
-          elementr.style.width = data.boundWidth;
-        // remoteVideoRef.current.offsetHeight = "1920";
-        // remoteVideoRef.current.offsetWidth = "1080";
-      });
+    //recive screen
+    socket.on("recive_sendscreen", (data) => {
+      console.log("Screen Data Arived So Long");
+      console.log(data.boundHeight);
+      console.log(data.boundWidth);
+      let elementr = document.getElementById("remscreen");
+      elementr.style.height = data.boundHeight;
+      elementr.style.width = data.boundWidth;
 
-
-
-  }, [socket]);
-
-  
-  useEffect(() => {
-    const remScreenElement = document.getElementById("remscreen");
-
-    remScreenElement.addEventListener("mousemove", (event) => {
-      var posX = event.offsetX;
-      var posY = event.offsetY;     
-      
-      var posXO = remScreenElement.offsetLeft;
-      var posYO = remScreenElement.offsetTop;
-      var clientX = event.pageX - posXO;
-      var clientY = event.pageY - posYO;
-
-      let remoteDimension = {
-        width: window.innerWidth,
-        height: window.innerHeight,
+      let ObjData = {
+        width: data.boundWidth,
+        height: data.boundHeight,
       };
 
-      const cwidth = remScreenElement.clientWidth;
-      const cheight = remScreenElement.clientHeight;
-      // const currentX = event.clientX
-      // const currentY = event.clientY
-      console.log("Offset X :" + posX);
-      console.log("Offset y :" + posY);
-      console.log("cheight" + cheight);
-      console.log("cwidth" + cwidth);
-
-      socket.emit("mousecord", {
-        clientX,
-        clientY,
-        posX,
-        posY,
-        remoteDimension,
-        room,
-        enableToggler,
-        cwidth,
-        cheight,
-      });
+      electron.screenResiser(ObjData);
+      // remoteVideoRef.current.offsetHeight = "1920";
+      // remoteVideoRef.current.offsetWidth = "1080";
     });
-  });
+  }, [socket]);
+
+  // useEffect(() => {
+  //   const remScreenElement = document.getElementById("remscreendiv");
+
+  //   remScreenElement.addEventListener("mousemove", (event) => {
+  //     var posX = event.offsetX;
+  //     var posY = event.offsetY;
+
+  //     var posXO = remScreenElement.offsetLeft;
+  //     var posYO = remScreenElement.offsetTop;
+  //     var clientX = event.pageX - posXO;
+  //     var clientY = event.pageY - posYO;
+
+  //     let remoteDimension = {
+  //       width: window.innerWidth,
+  //       height: window.innerHeight,
+  //     };
+
+  //     const cwidth = remScreenElement.clientWidth;
+  //     const cheight = remScreenElement.clientHeight;
+  //     // const currentX = event.clientX
+  //     // const currentY = event.clientY
+  //     console.log("Offset X :" + posX);
+  //     console.log("Offset y :" + posY);
+  //     console.log("cheight" + cheight);
+  //     console.log("cwidth" + cwidth);
+
+  //     socket.emit("mousecord", {
+  //       clientX,
+  //       clientY,
+  //       posX,
+  //       posY,
+  //       remoteDimension,
+  //       room,
+  //       enableToggler,
+  //       cwidth,
+  //       cheight,
+  //     });
+  //   });
+  // });
 
   // useeffect hook for peer js starts
   useEffect(() => {
@@ -231,15 +245,18 @@ function App() {
     electron.getScreenId((event, screenId) => {
       console.log(screenId);
       // getStream(screenId);
-    }); 
-    
-    
-    
+    });
+
     // get screen id
     electron.runCommand((event) => {
       console.log(event);
       console.log("Run Run");
       // getStream(screenId);
+    });
+
+    electron.sendRemClose((event) => {
+      socket.emit("screenshot", { hold, room });
+      console.log("close working yeh");
     });
 
     const handleStream = (stream) => {
@@ -258,8 +275,8 @@ function App() {
         call.on("stream", (remoteStream) => {
           remoteVideoRef.current.srcObject = remoteStream;
 
-          remoteVideoRef.onloadedmetadata = (e) => remoteVideoRef.current.play();
-           
+          remoteVideoRef.onloadedmetadata = (e) =>
+            remoteVideoRef.current.play();
         });
       });
 
@@ -323,20 +340,21 @@ function App() {
   });
 
   // // basic sampling code
-  // const handleMouseMove = ({ clientX, clientY }) => {
-  //   let clientWidth = remoteVideoRef.current.clientWidth;
-  //   let clientHeight = remoteVideoRef.current.clientHeight;
-  //   console.log(clientX, clientY);
-  //   socket.emit("mousecord", {
-  //     clientX,
-  //     clientY,
-  //     // clientWidth: window.innerWidth,
-  //     // clientHeight: window.innerHeight,
-  //     clientWidth,
-  //     clientHeight,
-  //     room,
-  //   });
-  // };
+  const handleMouseMove = ({ clientX, clientY }) => {
+    let clientWidth = remoteVideoRef.current.clientWidth;
+    let clientHeight = remoteVideoRef.current.clientHeight;
+    console.log(clientX, clientY + " : Hey mouse moved");
+    socket.emit("mousecord", {
+      clientX,
+      clientY,
+      // clientWidth: window.innerWidth,
+      // clientHeight: window.innerHeight,
+      clientWidth,
+      clientHeight,
+      room,
+      enableToggler,
+    });
+  };
 
   function handlesamplethress(event) {
     localStorage.setItem("sampling_thresold", event.target.value);
@@ -396,15 +414,14 @@ function App() {
   // console.log(clientWidth + "Hey its width");
   // console.log(clientHeight + "Hey its height");
 
+  //screenshot event sockett
+  const getImage = () => {
+    socket.emit("screenshot", { hold, room });
+  };
 
-//screenshot event sockett
-const getImage = () => {
-socket.emit("screenshot", { hold, room });
-}
-
-
-
-
+  const closeRemoteWindow = () => {
+    socket.emit("sendquiteapp", { hold, room });
+  };
 
   // click and hold mouse events
   let hold;
@@ -428,20 +445,19 @@ socket.emit("screenshot", { hold, room });
     document.addEventListener("keydown", handleKeyDown, true);
     // document.addEventListener("keyup", handleKeyUp, true);
   });
-  
+
   const handleKeyDown = (e) => {
     let key = e.key;
     console.log(key);
 
-      let modifire = "up";
-    socket.emit("qwert_keys", { key, room, modifire , keyHoldMode });
-  }; 
-  
-  
+    let modifire = "up";
+    socket.emit("qwert_keys", { key, room, modifire, keyHoldMode });
+  };
+
   // const handleKeyUp = (e) => {
   //   let key = e.key;
   //   console.log(key + " up");
-    
+
   //   if ((key = "`")) {
   //     console.log("template literel");
   //   }
@@ -450,55 +466,148 @@ socket.emit("screenshot", { hold, room });
   //   socket.emit("qwert_keys", { key, room, keyHoldMode, modifire });
   // };
 
-
-
   const handleChange = () => {
     setIsMouseHide(!isMouseHide);
   };
 
+  // turn on mon
 
+  // const handleMonOn = () =>{
+  // socket.emit("monon", { room })
+  // }
 
- 
+  // // turn of mon
 
+  // const handleMonOff = () =>{
+  // socket.emit("monoff", { room })
+  // }
 
+  // panel auto hide
 
+  // useeffect for firebase
+  useEffect(() => {
+    const userCollectionRef = collection(db, "userstatus");
 
+    onSnapshot(userCollectionRef, (snapshot) => {
+      const filteredData = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      console.log(filteredData + "Fil");
+      setUserList(filteredData);
+    });
+  }, []);
 
-// turn on mon
+  const changePeerState = async (userpeer, userid) => {
+    setIsPeerBusy(true);
+    const datatoupload = {
+      peerid: userpeer,
+      isOnline: true,
+      isPeerBusy: isPeerBusy,
+    };
+    try {
+      const document = doc(firestoredb, "userstatus", userid);
+      let dataUpdated = await setDoc(document, datatoupload);
+      console.log(dataUpdated + "data updated");
+    } catch (error) {
+      console.log("Error firebase :" + error);
+    }
+  };
 
-
-// const handleMonOn = () =>{
-// socket.emit("monon", { room })
-// }
-
-
-// // turn of mon
-
-// const handleMonOff = () =>{
-// socket.emit("monoff", { room })
-// }
-
-
-
-// panel auto hide
-
-
-
-useEffect(()=>{
-
-
-})
-
-
-
-
-
-
-
+  //user picker pin
 
   return (
     <>
-      <div id="bg" onMouseLeave={() => {}}>
+      <div
+        id="bg"
+        onClick={() => {
+          if (selecctorBox.current.style.visibility == "visible") {
+            setTimeout(() => {
+              selecctorBox.current.style.visibility = "hidden";
+            }, 5000);
+          } else {
+          }
+        }}
+        onMouseLeave={() => {}}
+      >
+        <div
+          ref={selecctorPin}
+          onClick={() => {
+            if (selecctorBox.current.style.visibility == "visible") {
+              setTimeout(() => {
+                selecctorBox.current.style.visibility = "hidden";
+              }, 5000);
+            } else {
+              selecctorBox.current.style.visibility = "visible";
+            }
+          }}
+          className="pin_iselector"
+        >
+          <div ref={selecctorBox} className="box_idselector">
+            <div className="userlistiterator">
+              {userList.map((user) => (
+                <div
+                  style={
+                    user.isPeerBusy
+                      ? {
+                          backgroundColor: "#EC9007",
+                          border: "1px solid #FFF5E7",
+                          boxShadow: "0px 1px 1.5px 0px rgba(0, 0, 0, 0.25)",
+                          overflow: "hidden",
+                        }
+                      : {
+                          backgroundColor: "#00CF6C",
+                          border: "1px solid #FFF5E7",
+                          boxShadow: "0px 1px 1.5px 0px rgba(0, 0, 0, 0.25)",
+                          overflow: "hidden",
+                        }
+                  }
+                  className="userbox"
+                >
+                  <div className="userbox_child1">
+                    <span> User Connection {count++} </span>
+
+                    {user.isPeerBusy ? (
+                      <span> Status : Busy </span>
+                    ) : (
+                      <span> Status : Available </span>
+                    )}
+
+                    {/* <p>{user.peerid}</p> */}
+                  </div>
+
+                  <button
+                    style={
+                      user.isPeerBusy
+                        ? {
+                            backgroundColor: "#EC7507",
+                            border: "1px solid #F8A120",
+                          }
+                        : {
+                            backgroundColor: "#69FF78",
+                            border: "1px solid #9BFF6B",
+                          }
+                    }
+                    id="connectbtn"
+                    onClick={(e) => {
+                      setRoom(user.peerid);
+                      // setRoom(user.peerid);
+                      // joinRoom();
+                      // changePeerState(user.peerid, user.id);
+                      changePeerState(user.peerid, user.id);
+                      joinRoom();
+                      // navigator.clipboard.writeText(user.peerid);
+                    }}
+                  >
+                    Connect To Remote
+                  </button>
+                  {/* <button id='deldata' onClick={deleteData}>Delete </button> */}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
         <div
           className="controlpin"
           id="controlpin"
@@ -548,7 +657,7 @@ useEffect(()=>{
             setIsMouseOnPanel(false);
           }}
         >
-          <input
+          {/* <input
             type="text"
             id="roomInput"
             ref={inputRoom}
@@ -556,7 +665,7 @@ useEffect(()=>{
           />
           <button onClick={joinRoom} id="connect_user">
             Connect User{" "}
-          </button>
+          </button> */}
           {/* <input type="text" id='input' ref={inputValue} onChange={(e)=>{ setMessage(e.target.value)}} placeholder='enter message ' /> */}
           {/* <button onClick={sendMessage} >Send Message</button> */}
           {/* <div className="samplethressdiv">
@@ -598,6 +707,11 @@ useEffect(()=>{
           </div> */}
           <div className="toggerdiv">
             <button
+              style={
+                enableToggler
+                  ? { backgroundColor: "#06aa5d" }
+                  : { backgroundColor: "#aa0634" }
+              }
               className="toggerdiv_button"
               onClick={() => {
                 if (enableToggler == false) {
@@ -607,12 +721,16 @@ useEffect(()=>{
                 }
               }}
             >
-              Alternet Mouse
+              Enable Toggler
             </button>
           </div>
           <div>
-            <button onClick={getImage}>Capture Screenshot</button>
+            {/* <button onClick={getImage}>Capture Screenshot</button> */}
           </div>
+          <div>
+            <button onClick={getImage}>Kill Remote</button>
+          </div>
+
           {/* <button onClick={handleMonOff}>Mon Off</button>
           <button onClick={handleMonOn}>Mon On</button> */}
           {/* 
@@ -632,17 +750,17 @@ useEffect(()=>{
           className="remscreendiv"
           onMouseDown={handleMouseDown}
           onMouseUp={handleMouseUp}
-          // onClick={handleMouseClick}
-          // onMouseMove={handleMouseMove}
+          onClick={handleMouseClick}
+          onMouseMove={handleMouseMove}
           ref={videoarearef}
           onContextMenu={(event) => {
             if (event.button === 2) {
               event.preventDefault();
-              socket.emit("mouseclickr", { room });
+              socket.emit("mouseclickr", { room, enableToggler });
               console.log("You right-clicked on the element!");
             }
           }}
-          >
+        >
           <video
             // onMouseMove={handleMouseMove}
             id="remscreen"
