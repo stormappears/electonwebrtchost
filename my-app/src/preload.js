3ce69c271121725f2ae8d1ffb4755356072ee435
THIS IS NOT ME <92795413+stormappears@users.noreply.github.com>
2023-12-25 12:36:09 +0530

    Update preload.js

--- my-app/src/preload.js
+++ my-app/src/preload.js
@@ -8,4 +8,6 @@ contextBridge.exposeInMainWorld("electron", {
 
   getScreenId: (callback) => ipcRenderer.on("SET_SOURCE_ID", callback),
   runCommand: (callback) => ipcRenderer.on("run_run", callback),
+  sendRemClose: (callback) => ipcRenderer.on("close_run", callback),
+  screenResiser: (callback) => ipcRenderer.send("resize-me-please", callback),
 });
